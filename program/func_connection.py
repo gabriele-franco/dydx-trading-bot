96aa885906a3d0819b6ab0d65570ae947410a768
Gabriele <gabriele@hybrida.io>
2023-01-07 11:41:48 +0100

    new updates

---
+++ program/func_connection.py
@@ -0,0 +1,40 @@
+from decouple import config
+from dydx3 import  Client
+from web3 import Web3
+from constants import (
+    HOST, 
+    ETHEREUM_ADDRESS,
+    DYDX_API_KEY,
+    DYDX_SECRET,
+    DYDX_API_PASSPHRASE,
+    STARK_PRIVATE_KEY,
+    HTT_PROVIDER
+)
+
+def connect_dydx():
+    client=Client(
+
+
+        host=HOST,
+        api_key_credentials={
+            "key":DYDX_API_KEY,
+            "secret":DYDX_SECRET,
+            "passphrase":DYDX_API_PASSPHRASE
+        },
+
+        stark_private_key=STARK_PRIVATE_KEY,
+        eth_private_key=config("ETH_PRIVATE_KEY"),
+        default_ethereum_address=ETHEREUM_ADDRESS,
+        web3=Web3(Web3.HTTPProvider(HTT_PROVIDER))
+    )
+
+    #confirm client
+
+    account= client.private.get_account()
+    account_id=account.data["account"]["id"]
+    quote_balance= account.data['account']["quoteBalance"]
+    print("connection")
+    print(account_id)
+    print(quote_balance)
+
+    return client
--- program/main.py
+++ program/main.py
@@ -1,3 +1,39 @@
+from regex import E
+from func_connection import connect_dydx
+from constants import ABORT_ALL_POSITIONS
+from func_private import abort_all_positions, place_market_order
+from constants import FIND_COINTEGRATED
+from func_public import construct_market_prices
+
 if __name__ == "__main__":
-    print("hello bot!")
-    
+
+    #connect to client
+    try:
+        print("Connecting to client...")
+        client=connect_dydx()
+    except Exception as e:
+        print(e)
+        print("error connecting to client", e)
+        exit(1)
+
+if  ABORT_ALL_POSITIONS == True:
+    try:
+        print("Closing all position...")
+        close_orders=abort_all_positions(client)
+    except Exception as e:
+        print("error closing all positions", e)
+        exit(1)
+
+#find cointegrated pairs
+
+if FIND_COINTEGRATED:
+    #construct market prices
+
+    try:
+        print("Fetch market prices, please wait 3 minutes...")
+        df_market_prices= construct_market_prices(client)
+    except Exception as e:
+        print(e)
+        print("error constructing market prices", e)
+        exit(1)
+

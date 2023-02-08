37bd6057d9bb9da401b8dd79af720e97824639c8
Gabriele <gabriele@hybrida.io>
2023-02-08 10:21:40 +0100

    solving streamlit

--- program/dashboard.py
+++ program/dashboard.py
@@ -1,9 +1,9 @@
 import streamlit as st 
 import pandas as pd 
 
-storico= pd.read_json('storico.json')
+storico= pd.read_json('/home/ubuntu/storico.json')
 
-bot_agent=pd.read_json('bot_agents.json')
+bot_agent=pd.read_json('/home/ubuntu/bot_agents.json')
 
 st.dataframe(storico)
 st.dataframe(bot_agent)

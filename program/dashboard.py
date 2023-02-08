0c9e7c82371b79535e09167eeafba89d91799f1e
Gabriele <gabriele@hybrida.io>
2023-02-08 10:55:54 +0100

    modifying the dashboard

--- program/dashboard.py
+++ program/dashboard.py
@@ -2,8 +2,18 @@ import streamlit as st
 import pandas as pd 
 
 storico= pd.read_json('/home/ubuntu/storico.json')
-
 bot_agent=pd.read_json('/home/ubuntu/bot_agents.json')
+storico['profit']=storico['base_tot']+storico['quote_tot']
 
+total_profit=storico['profit'].sum()
+open_positions=len(bot_agent['pair_status'])*2
+
+st.title('total profit is {total_profit}')
+st.title('the number of live orders is {open_positions}')
+st.write('this is the storico ordini')
 st.dataframe(storico)
+
+st.write('these should be the live orders')
+st.title('bot_agent')
+
 st.dataframe(bot_agent)
